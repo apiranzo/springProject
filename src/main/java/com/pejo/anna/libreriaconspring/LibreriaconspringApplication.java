d67c0c065963fc5ee2a5e7ec089f7b7ecff08645
apiranzo <annapejo@gmail.com>
2024-04-24 09:50:09 +0200

    Creación de Spring Project

---
+++ src/main/java/com/pejo/anna/libreriaconspring/LibreriaconspringApplication.java
@@ -0,0 +1,13 @@
+package com.pejo.anna.libreriaconspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaconspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaconspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pejo/anna/libreriaconspring/LibreriaconspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pejo.anna.libreriaconspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaconspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
