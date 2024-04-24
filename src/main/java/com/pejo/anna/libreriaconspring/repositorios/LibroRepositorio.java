830e736daf8493ca3224c4d1c5865615590820ca
apiranzo <annapejo@gmail.com>
2024-04-24 10:45:44 +0200

    La implementación y el DAO base ya lo crea automaticamente

--- src/main/java/com/pejo/anna/libreriaconspring/repositorios/LibroRepositorio.java
+++ src/main/java/com/pejo/anna/libreriaconspring/repositorios/LibroRepositorio.java
@@ -1,5 +1,12 @@
 package com.pejo.anna.libreriaconspring.repositorios;
 
-public interface LibroRepositorio {
+import org.springframework.data.repository.CrudRepository;
+
+import com.pejo.anna.libreriaconspring.entidades.Libro;
+
+public interface LibroRepositorio extends CrudRepository<Libro, Long>{
+	//La crea Spring de forma embevida 
+	//De forma automática CrudRepository es el DAO<T> que hacíamos con operaciones básicas
+	//Cuando arranca spring se crea el codigo en tiempo real. 
 
 }
